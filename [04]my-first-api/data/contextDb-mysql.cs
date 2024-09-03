fe0f8a341514796bb60a743c8c6cd590a666245b
emmanuelajsivinac <emmajsivinac@gmail.com>
2024-09-03 12:13:00 -0600

    update Files

---
+++ [04]my-first-api/data/contextDb-mysql.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using mysql.Models;
+
+namescpace mysql
+{
+    
+}
+public class DatabaseContextMysql:DbContext
+{
+    public DbS
+}
---
+++ [04]my-first-api/models/test.cs
@@ -0,0 +1,7 @@
+namespace [04]My-FIRST-API.Models
+{
+    public class Test{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    }
+}
